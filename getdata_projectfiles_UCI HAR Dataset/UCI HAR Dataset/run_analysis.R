1eb6c9e066570c5db8c673eab839c0e61d94386b
rennyatwork <rennyatwork@gmail.com>
2014-09-08 21:40:10 -0400

    commiting run_analysis
    merges train and test datasets
    rename columns

--- getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/run_analysis.R
+++ getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/run_analysis.R
@@ -9,5 +9,14 @@
 # trainDf <- read.table("train/train_X.csv", header=FALSE, sep=",")
 # ncol(trainDf)
 
+#trainAndTestDf <- merge(testDf, trainDf, all=T)
 
+#setwd("C:\\Users\\renato\\Documents\\MOOC\\2014\\course03 - DataCleaning\\Assignment-01\\getdata_projectfiles_UCI HAR Dataset\\UCI HAR Dataset")
+#headersDf <- read.table("features.txt", header=F, sep="\n")
 
+#cutting off numbers before colum names
+#sub("\\d+ ", "", headersDf[,1])
+#newColNames <- sub("\\d+ ", "", headersDf[,1])
+
+#renaming columns in trainAndTestDf
+#colnames(trainAndTestDf) <- newColNames
