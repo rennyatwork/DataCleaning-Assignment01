4e78f1c6518f4e4d0f481982f0d6f35186f3ab55
rennyatwork <rennyatwork@gmail.com>
2014-09-08 20:49:30 -0400

    first commit

---
+++ getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/assign-01.R
@@ -0,0 +1,13 @@
+#require(dplyr)
+# setwd("C:\\Users\\renato\\Documents\\MOOC\\2014\\course03 - DataCleaning\\Assignment-01\\getdata_projectfiles_UCI HAR Dataset\\UCI HAR Dataset\\")
+
+#loading test data
+#testDf <- read.table("test/test_X.csv", header=FALSE, sep=",")
+#ncol(testDf)
+
+#loading training data
+# trainDf <- read.table("train/train_X.csv", header=FALSE, sep=",")
+# ncol(trainDf)
+
+
+
