2855b760c5dd32930817cae2397f91763fefb728
rennyatwork <rennyatwork@gmail.com>
2014-09-13 12:52:30 -0400

    now merging all datasets correctly
    fully merging train and tests datasets

--- getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/run_analysis.R
+++ getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/run_analysis.R
@@ -17,11 +17,15 @@ loadFile <- function(pFileName)
 mergeTrainAndTest <- function()
 {
   #test_x.csv is a modified file
-  #testDf<- read.table("./test/test_X.csv", header=FALSE, sep=",")
-  testDf <- getMergedTrainOrTest("test")
+  testDf<- read.table("./test/test_X.csv", header=FALSE, sep=",")
+  #testDf <- getMergedTrainOrTest("test")
   
-  #trainDf <- read.table("./train/train_X.csv", header=FALSE, sep=",")
-  trainDf <- getMergedTrainOrTest("train")
+  trainDf <- read.table("./train/train_X.csv", header=FALSE, sep=",")
+  #trainDf <- getMergedTrainOrTest("train")
+  
+  mainMerge <- getMergedTrainAndTest("MAIN")
+  labelsMerge <- getMergedTrainAndTest("LABELS")
+  subjectssMerge <- getMergedTrainAndTest("SUBJECTS")
   
   #print("testDf")
   #print(ncol(testDf))
@@ -29,7 +33,7 @@ mergeTrainAndTest <- function()
   #print("trainDf")
   #print(ncol(trainDf))
   
-  trainAndTestDf <- merge(testDf, trainDf)
+  trainAndTestDf <- merge(testDf, trainDf, all=T)
   
   return(trainAndTestDf)
 }
@@ -69,6 +73,61 @@ getMergedTrainOrTest <- function(pTrainOrTest)
   return (mainDf)
 }
 
+############################
+## returns the X_[Train|Test].txt
+getMainFileName <-function(pTrainOrTest)
+{
+  return (paste( paste(pTrainOrTest, paste("/X_", pTrainOrTest, sep=""), sep=""), ".txt", sep=""))
+}
+
+## returns the file path to y_[train|test].txt
+getLabelsFileName <-function(pTrainOrTest)
+{
+  return(paste( paste(pTrainOrTest,paste("/y_", pTrainOrTest, sep=""), sep=""), ".txt", sep=""))
+}
+
+## returns subject_[train|test] filename
+getSubjectsFilenaName <-function(pTrainOrTest)
+{
+  return(paste( paste(pTrainOrTest,paste("/subject_", pTrainOrTest, sep=""), sep=""), ".txt", sep=""))
+}
+###########################
+
+### This functions reads files from either Train or Test
+### and returns a merged dataset (train with test, subjectsTrain with sbujectsTest, labelsTrain with labelsTest)
+getMergedTrainAndTest <- function(pDataSet)
+{
+  
+  if (pDataSet=="MAIN")
+  {
+    #file containing the 561 columns
+    mainTrainDf <- loadFile(getMainFileName("Train"))
+    mainTestDf <- loadFile(getMainFileName("Test"))
+    #return (merge(mainTrainDf, mainTestDf, all=T))
+    return (rbind(mainTrainDf, mainTestDf))
+  }
+  
+  
+  else if (pDataSet=="LABELS")
+  {
+    #labels from 1 to 6
+    labelsTrainDf <- loadFile(getLabelsFileName("Train"))
+    labelsTestDf <- loadFile(getLabelsFileName("Test"))
+    return (rbind(labelsTrainDf, labelsTestDf))
+  }
+  
+  else if (pDataSet=="SUBJECTS")
+  {
+    #labels from 1 to 6
+    subctsTrainDf <- loadFile(getSubjectsFilenaName("Train"))
+    subjectsTestDf <- loadFile(getSubjectsFilenaName("Test"))
+    #return (merge(subctsTrainDf, subjectsTestDf, all=T))
+    return (rbind(subctsTrainDf, subjectsTestDf))
+  }
+  
+ 
+}
+
 
 ## 2 - returns dataframe containing only measurements on mean and std
 getOnlyMeanAndStdDf <-function(pTrainAndTestDf)
@@ -90,10 +149,40 @@ getOnlyMeanAndStdDf <-function(pTrainAndTestDf)
 }
 
 
+
+####
+#3 Horizontaly merged = adding subject and label columns
+getHorizontalyMergedDf <-function(pMainDf, pLabelsDf, pSubjectsDf)
+{
+  dfActivityLabels <- loadFile("activity_labels.txt")
+ 
+  
+  #merging all the activities with the corresponding labels
+  dfLabelsNames <- merge(pLabelsDf, dfActivityLabels)
+  
+  #adding the subject column
+  pMainDf$subject <- pSubjectsDf
+  
+  #adding the activty code (1-6)
+  pMainDf$ActivityCode <- dfLabelsNames[, 1]
+  
+  #adding the activity name
+  pMainDf$TrainActivityLabel <- dfLabelsNames[, 2]
+  
+  return(pMainDf)
+}
+
 #initializeEnvironment()
 #trainAndTestDf <- mergeTrainAndTest()
 #reducedDf <- getOnlyMeanAndStdDf(trainAndTestDf)
 #joinedReducedDf <- tbl_df(reducedDf)
+
+
+#mainMerge <- getMergedTrainAndTest("MAIN")
+#labelsMerge <- getMergedTrainAndTest("LABELS")
+#subjectsMerge <- getMergedTrainAndTest("SUBJECTS")
+#mainMerge <- getOnlyMeanAndStdDf(mainMerge)
+#fullyMerged <- getHorizontalyMergedDf(mainMerge, labelsMerge, subjectssMerge)
 ##################
 
 #require(dplyr)
